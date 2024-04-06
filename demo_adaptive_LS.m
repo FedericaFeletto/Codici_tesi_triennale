5a208f0bbe90536eec5331aec300dbf1c099bde4
Federica Feletto <165161881+FedericaFeletto@users.noreply.github.com>
2024-04-06 14:22:17 +0200

    Add files via upload

--- demo_adaptive_LS.m
+++ demo_adaptive_LS.m
@@ -1,14 +1,13 @@
-
-function demo_adaptive(example)
+function demo_adaptive_LS(example)
 
 %--------------------------------------------------------------------------
-% OBJECT:
+% OBJECT
 %--------------------------------------------------------------------------
 % This demo shows how to estimate the Lebesgue constant and its relative
-% error at degree "deg" of the interpolation at pointset "pts" in a certain
+% error at degree "deg" of LS-AM pointset "pts" in a certain
 % domain, with a fixed tolerance.
 %--------------------------------------------------------------------------
-% INPUT:
+% INPUT
 %--------------------------------------------------------------------------
 %
 % example: the variable defines the following domains
@@ -77,12 +76,12 @@ function demo_adaptive(example)
 %--------------------------------------------------------------------------
 
 % SETTINGS.
-if nargin < 1, example=14; end
+if nargin < 1, example=1; end
 
 warning off;
 tol= 10^(-2); % Lebesgue constant relative error tolerance.
-m_pts=4; % Constant of the AM set used for intp. points extraction.
-maxdeg=20;
+mindeg=1;
+maxdeg=50;
 save_domain_plots=1; % 1. save figs with domain
 
 
@@ -96,104 +95,61 @@ den=2*acos(1/(1+tol));
 m=ceil(pi/den);
 
 % EXPERIMENTS RUN.
-degV=1:maxdeg;
-lejaV=0:2;
-AM_cost=zeros(degV(end),3);
-lebV_est=zeros(degV(end),3);
-lebV_LB=zeros(degV(end),3);
-lebV_UB=zeros(degV(end),3);
-pts_leja={}; 
+degV=mindeg:maxdeg;
+AM_cost=zeros(degV(end),1);
+lebV_LB=zeros(degV(end),1);
+lebV_UB=zeros(degV(end),1);
+lebV_est=zeros(degV(end),1);
 
-for leja=lejaV
 
-    % Displays.
-    fprintf('\n \t * Running tests on: ');
-    switch leja
-        case 0
-            set_leja_L='AFP';
-        case 1
-            set_leja_L='DLP';
-        case 2
-            set_leja_L='PLP';
-    end
-    disp(set_leja_L);
-    zAM_forall_deg={};
-    pts_forall_deg={};
 
-    % Actual experiments.
-    for deg=degV
-        [pts,zAM_pts,domain]=interp_pointset(domain,leja,deg,m_pts);
-        [leb_constant,zAM,C,domain]=evaluate_leb_const(domain,pts,deg,m);
-        leb_LB=leb_constant;
-        leb_UB=C*leb_LB;   
-        lebL_est=(leb_LB+leb_UB)/2; % estimating Lebesgue constant for AFP/DLP/PLP
-        lebV_est(deg,leja+1)=lebL_est;
-        lebV_LB(deg,leja+1)=leb_LB;
-        lebV_UB(deg,leja+1)=leb_UB;
-        AM_cost(deg,leja+1)=C;
-        zAM_forall_deg{end+1}=zAM;
-        pts_forall_deg{end+1}=pts;
-        
+% Actual experiments.
+for deg=degV
 
-        fprintf('\n \t -> deg: %2.0f leb: %1.3e  m_pts: %2.0f m: %2.0f  AM#: %6.0f',...
-            deg,leb_constant,m_pts,m,length(zAM));
+    % Below:
+    % leb_LB, leb_UB: Lebesgue constant bounds
+    % lebL_est      : (leb_LB+leb_UB)/2
+    % leb_LB  : fine approximation of Lebesgue constant.
 
-    end
+    pts=complex_AM(domain,deg,m);
+    [leb_LB,zAM,C,domain]=evaluate_leb_const(domain,pts,deg,m);
+    leb_UB=C*leb_LB;
+    lebL_est=(leb_LB+leb_UB)/2; % estimating Lebesgue constant
+    lebV_est(deg,1)=lebL_est;
+    lebV_LB(deg,1)=leb_LB;
+    lebV_UB(deg,1)=leb_UB;
+    AM_cost(deg,1)=C;
 
-    zAM_forall_deg{end+1}=zAM_forall_deg; 
-    pts_leja{end+1}=pts_forall_deg;
-
-    % Results
-    fprintf('\n \n \t ........................ Summary ........................');
-
-    fprintf('\n \t Example                    : %2.0f',example);
-    fprintf('\n \t Pointset                   : %s',set_leja_L);
-    fprintf('\n \t Max Degree                 : %2.0f',degV(end));
-    fprintf('\n \t Required tolerance (tol)   : %3.6f',tol);
-    fprintf('\n \t AM factor (m_pts)          : %2.0f',m_pts);
-    fprintf('\n \t AM card for maxdeg         : %5.0f',length(zAM_forall_deg{1,maxdeg}));
-    fprintf('\n \t .........................................................');
-    fprintf('\n \n');
+    fprintf('\n \t -> deg: %3.0f leb: [%1.3e , %1.3e]  m: %3.0f  AM#: %7.0f',...
+        deg,leb_LB,leb_UB,m,length(zAM));
 
 end
 
+% Results.
+
+fprintf('\n \n \t .............. Summary ...............');
+fprintf('\n \t Pointset AM');
+fprintf('\n \t Example                    : %2.0f',example);
+fprintf('\n \t Max Degree                 : %3.0f',degV(end));
+fprintf('\n \t Required tolerance (tol)   : %1.5f',tol);
+fprintf('\n \t ......................................');
+fprintf('\n \n');
 
 % STATISTICS.
-fprintf('\n \t ...................AFP..................');
+
+fprintf('\n \t ..................LS..................');
 fprintf('\n \t Max Degree                  : %3.0f',degV(end));
 fprintf('\n \t Leb. const. est. for maxdeg : %3.6f',lebV_est(maxdeg,1));
 fprintf('\n \t Leb. const. LB   for maxdeg : %3.6f',lebV_LB(maxdeg,1));
 fprintf('\n \t Leb. const. UB   for maxdeg : %3.6f',lebV_UB(maxdeg,1));
 fprintf('\n \n');
 
-fprintf('\n \t ...................DLP..................');
-fprintf('\n \t Max Degree                  : %3.0f',degV(end));
-fprintf('\n \t Leb. const. est. for maxdeg : %3.6f',lebV_est(maxdeg,2));
-fprintf('\n \t Leb. const. LB   for maxdeg : %3.6f',lebV_LB(maxdeg,2));
-fprintf('\n \t Leb. const. UB   for maxdeg : %3.6f',lebV_UB(maxdeg,2));
-fprintf('\n \n');
-
-fprintf('\n \t ...................PLP..................');
-fprintf('\n \t Max Degree                  : %3.0f',degV(end));
-fprintf('\n \t Leb. const. est. for maxdeg : %3.6f',lebV_est(maxdeg,3));
-fprintf('\n \t Leb. const. LB   for maxdeg : %3.6f',lebV_LB(maxdeg,3));
-fprintf('\n \t Leb. const. UB   for maxdeg : %3.6f',lebV_UB(maxdeg,3));
-fprintf('\n \n');
-
-
-% Type of pointset in figure 1:
-Leja=2;  % 1: AFP, 2: DLP, 3: PLP
-set_Leja=pts_leja{Leja};
-pts=set_Leja{end};
-
-
 % PLOT.
 
-% A. Lebesgue constants plots
+% A. Plot pointset
 clear_figure(1);
 fig=figure(1);
-plot(real(zAM_pts),imag(zAM_pts),'.','color',[0.9290 0.6940 0.1250],'MarkerSize',4); hold on;
-plot(real(pts),imag(pts),'o','MarkerEdgeColor','k','MarkerFaceColor','c','MarkerSize',6);
+plot(real(zAM),imag(zAM),'.','color',[0.9290 0.6940 0.1250],'MarkerSize',4); hold on;
 axis equal;
 axis off;
 axis tight;
@@ -202,27 +158,24 @@ set(gca,'ytick',[])
 hold off;
 
 if save_domain_plots
-print(fig,'example.eps','-depsc');
-savefig('example.fig');
+print(fig,'example_LS.eps','-depsc');
+savefig('example_LS.fig');
 end
 
-
-% B. Plot domain and pointsets
+% B. Lebesgue constants plots
 clear_figure(2);
 fig=figure(2);
-plot(degV(:),lebV_est(:,1),'m','Linewidth',2); hold on;
-plot(degV(:),lebV_est(:,2),'color',[0.4940 0.1840 0.5560],'Linewidth',2); hold on;
-plot(degV(:),lebV_est(:,3),'g','Linewidth',2); hold on;
+semilogy(degV(:),lebV_est(:,1),'color',[0.6350 0.0780 0.1840],'Linewidth',2); hold on;
 title('Lebesgue constant estimate');
 xlabel('Degree'); ylabel('Lebesgue constants approximation');
-legend('AFP','DLP','PLP','Location','northwest');
 grid on;
 hold on;
 
 if save_domain_plots
-print(fig,'leb_const_est.eps','-depsc');
-savefig('leb_const_est.fig');
+print(fig,'leb_const_est_LS.eps','-depsc');
+savefig('leb_const_est_LS.fig');
 end
+
 
 
 function clear_figure(nfig)
@@ -232,4 +185,3 @@ f_nfig=ishandle(h)&&strcmp(get(h,'type'),'figure');
 if f_nfig
     clf(nfig);
 end
-
